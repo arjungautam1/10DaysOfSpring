1bd36af904616015b719a689e7bfabb0ffb18df5
Laserarjun876 <laserarjun876@gmail.com>
2019-10-03 08:24:38 +0545

    first commit

---
+++ src/Day001/ArrayDeclaration.java
@@ -0,0 +1,15 @@
+package Day001;
+//For array Declaration in simple way
+public class ArrayDeclaration {
+	public static void main(String[] args) {
+		int[] array= {1,2,3,4,5,6,7,8,9,10};	//declaration
+		int index=0;
+		while(index<5)
+		{
+			System.out.println("Number in index "+index+" is :"+array[index]);
+			index=index+1;
+		}
+		
+	}
+
+}
---
+++ src/Day001/ForLoopInArray.java
@@ -0,0 +1,14 @@
+package Day001;
+//Using for loop in array in easy way
+public class ForLoopInArray {
+	public static void main(String[] args) {
+		int[] array= {1,2,3,4,5,6,7,8,9};
+		
+		for(int element:array)
+		{
+			System.out.println(element);
+			
+		}
+	}
+
+}
