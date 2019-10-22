750cd75771268ee2c9dd524d7595153a1bbdd736
Laserarjun876 <laserarjun876@gmail.com>
2019-10-22 22:00:08 +0545

    Spring Data Rest

---
+++ src/Day011/target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(1,'Arjun','Java');
+insert into alien values(2,'Hari','AI');
+insert into alien values(3,'BJ','Kotlin');
+insert into alien values(4,'Sita','Machine Learning');
+
